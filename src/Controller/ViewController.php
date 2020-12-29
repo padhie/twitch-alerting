3f1cde01245a0c0aa2d28d3ac1c2e47e87622308
PZ <padhiez@hotmail.de>
2020-12-29 16:28:17 +0100

    add view to play sound

---
+++ src/Controller/ViewController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AlertRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class ViewController extends AbstractController
+{
+    private AlertRepository $alertRepository;
+    private UserRepository $userRepository;
+
+    public function __construct(AlertRepository $alertRepository, UserRepository $userRepository)
+    {
+        $this->alertRepository = $alertRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @Route("/view/{userId}/", name="view")
+     */
+    public function index(string $userId, Request $request): Response
+    {
+        $user = $this->userRepository->findOneBy([
+            'id' => $userId,
+        ]);
+        if ($user === null) {
+            $this->redirect('frontend');
+        }
+
+        $alerts = $this->alertRepository->findBy([
+            'user' => $user,
+            'active' => true,
+        ]);
+
+        return $this->render('view/index.html.twig', [
+            'alerts' => $alerts,
+            'user' => $user,
+            'baseFileUrl' => '/sound/' . $user->getId(),
+            'debug' => (bool) $request->get('debug', false),
+        ]);
+    }
+}
---
+++ templates/view/main.js
@@ -0,0 +1,190 @@
+const audioPattern = `
+    <audio id="{ID}" class="hidden" controls>
+        <source src="{SRC}" type="audio/mp3" />
+        Your browser does not support the audio element.
+    </audio>
+`;
+const topics = [
+    'channel-points-channel-v1.{{ user.getTwitchId() }}',
+];
+
+let ws;
+let htmlOutput = false;
+let twitchOAuthToken = '{{ user.getTwitchOAuth() }}';
+
+let sounds = [];
+let queue = [];
+
+function init() {
+    connect();
+
+    setTimeout(
+        () => {
+            for (let topic of topics) {
+                listen(topic);
+            }
+        },
+        1000
+    );
+
+
+    setInterval(playQueue, 1000);
+}
+
+function connect() {
+    const heartbeatInterval = 1000 * 60; //ms between PING's
+    const reconnectInterval = 1000 * 3; //ms to wait before reconnect
+    let heartbeatHandle;
+
+    ws = new WebSocket('wss://pubsub-edge.twitch.tv');
+
+    ws.onopen = function() {
+        output('INFO', 'Socket Opened');
+        heartbeat();
+        heartbeatHandle = setInterval(heartbeat, heartbeatInterval);
+    };
+
+    ws.onerror = function(error) {
+        output('ERR', JSON.stringify(error));
+    };
+
+    ws.onmessage = function(event) {
+        let message = JSON.parse(event.data);
+
+        output('RECV', JSON.stringify(message));
+
+        if (message.type === 'MESSAGE') {
+            addSoundByResponse(message);
+        }
+
+        if (message.type === 'RECONNECT') {
+            output('INFO', 'Reconnecting...');
+            setTimeout(connect, reconnectInterval);
+        }
+    };
+
+    ws.onclose = function() {
+        output('INFO', 'Socket Closed');
+        clearInterval(heartbeatHandle);
+
+        output('INFO', 'Reconnecting...');
+        setTimeout(connect, reconnectInterval);
+    };
+}
+
+function heartbeat() {
+    let message = {
+        type: 'PING'
+    };
+
+    output('SENT', JSON.stringify(message));
+    ws.send(JSON.stringify(message));
+}
+
+function output(type, message) {
+    if (htmlOutput) {
+        let outputArea = document.getElementById('ws-output');
+        outputArea.append(`${type}: ${message}\n`);
+        outputArea.scrollTop = outputArea.scrollHeight;
+    } else {
+        console.log(`${type}: ${message}`);
+    }
+}
+
+function showHtmlOutput() {
+    htmlOutput = true;
+
+    document.getElementById('socket').classList.toggle('hidden');
+
+    document.getElementById('topic-form').onsubmit = function(event) {
+        listen(document.getElementById('topic-text').value);
+        event.preventDefault();
+    };
+}
+
+function listen(topic) {
+    let message = {
+        type: 'LISTEN',
+        nonce: nonce(15),
+        data: {
+            topics: [topic],
+            auth_token: twitchOAuthToken
+        }
+    };
+
+    output('SENT', JSON.stringify(message));
+    ws.send(JSON.stringify(message));
+}
+
+function nonce(length) {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
+
+function addSoundByResponse(response) {
+    let data = JSON.parse(response.data.message);
+
+    if (data.type === 'reward-redeemed') {
+        let rewardTitle = data.data.redemption.reward.title;
+
+        addToQueue(rewardTitle);
+    }
+}
+
+function addToQueue(rewardTitle) {
+    let soundElement = null;
+    for (let i = 0; i < sounds.length; i++) {
+        if (sounds[i].name === rewardTitle) {
+            soundElement = sounds[i];
+            break;
+        }
+    }
+
+    if (soundElement === null) {
+        return;
+    }
+
+    queue.push(soundElement);
+}
+
+function playQueue() {
+    let players = document.getElementsByTagName('audio');
+    if (players.length === 1) {
+
+        let soundFinish = players[0].ended;
+        if (soundFinish === false) {
+            return;
+        }
+    }
+
+    if (queue.length === 0) {
+        return;
+    }
+
+    let nextSoundElement = queue[0];
+    playSound(nextSoundElement);
+
+    queue.shift();
+}
+
+function playSound(soundElement) {
+    let audioWrapper = document.getElementById('js-audio-wrapper');
+    let audioPlayer = audioPattern;
+
+    if (soundElement.playerId === undefined || soundElement.src === undefined) {
+        return;
+    }
+
+    audioPlayer = audioPlayer.replace('{ID}', soundElement.playerId);
+    audioPlayer = audioPlayer.replace('{SRC}', soundElement.src);
+
+    audioWrapper.innerHTML = audioPlayer;
+
+    document.getElementById(soundElement.playerId).play();
+}
